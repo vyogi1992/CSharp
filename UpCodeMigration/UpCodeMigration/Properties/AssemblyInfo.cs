5e6b57c7518a937e3d804c1aef76497ff7b7ffa2
vyogi1992 <vyogi1992@gmail.com>
2016-08-09 20:41:00 +0530

    Initial commit

---
+++ UpCodeMigration/UpCodeMigration/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+///<summary> General Information about an assembly is controlled through the following 
+///<summary> set of attributes. Change these attribute values to modify the information
+///<summary> associated with an assembly.
+[assembly: AssemblyTitle("UpCodeMigration")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UpCodeMigration")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+///<summary> Setting ComVisible to false makes the types in this assembly not visible 
+///<summary> to COM components.  If you need to access a type in this assembly from 
+///<summary> COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+///<summary> The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a384e5d-5adb-46b0-8b3b-2010dff03f61")]
+
+///<summary> Version information for an assembly consists of the following four values:
+///<summary>
+///<summary>      Major Version
+///<summary>      Minor Version 
+///<summary>      Build Number
+///<summary>      Revision
+///<summary>
+///<summary> You can specify all the values or you can default the Build and Revision Numbers 
+///<summary> by using the '*' as shown below:
+///<summary> [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UpCodeMigration/UpCodeMigration/Source codes/FileLoad.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UpCodeMigration.Source_codes;
+using System.IO;
+
+using System.Data;
+using System.Data.OleDb;
+
+namespace UpCodeMigration.Source_codes
+{
+    ///<summary>Class to handle file loads</summary>
+    ///<remarks></remarks>
+    public static class  FileLoad{
+
+
+        ///<summary>Function to load train details file</summary>
+        ///<returns> True if the file is loaded correctly. False if not</returns>
+        ///<remarks></remarks>
+        public static bool loadTrainData()
+        {
+            GlobalVariables.TRAIN_DATA = File.ReadAllLines(GlobalVariables.BASE_FOLDER_PATH + GlobalVariables.CAD_FILENAME)
+                                           .Select(v => TrainData.FromCsv(v))
+                                           .ToList();
+
+            if (GlobalVariables.TRAIN_DATA.Count > 0) { return true; }
+            else                                      { return false;}
+        }
+
+
+
+        ///<summary>Function to load project details file</summary>
+        ///<returns> True if the file is loaded correctly. False if not</returns>
+        ///<remarks></remarks>
+        public static bool loadProjectData()
+        {
+            GlobalVariables.PROJECT_DATA = File.ReadAllLines(GlobalVariables.BASE_FOLDER_PATH + GlobalVariables.PROJECTS_FILENAME)
+                                           .Skip(1)
+                                           .Select(v => ProjectData.FromCsv(v))
+                                           .ToList();
+
+            if (GlobalVariables.PROJECT_DATA.Count > 0) { return true; }
+            else { return false; }
+
+        }
+
+
+
+        ///<summary>Function to load untouchable trains details file</summary>
+        ///<returns> True if the file is loaded correctly. False if not</returns>
+        ///<remarks></remarks>
+        public static bool loadUntTrainData()
+        {
+            GlobalVariables.UNT_TRAIN_DATA = File.ReadAllLines(GlobalVariables.BASE_FOLDER_PATH + GlobalVariables.UNTOUCHABLES_FILENAME)
+                                           .Select(v => UntTrainData.FromCsv(v))
+                                           .ToList();
+
+            if (GlobalVariables.UNT_TRAIN_DATA.Count > 0) { return true; }
+            else { return false; }
+
+        }
+
+
+
+        ///<summary>Function to load input files</summary>
+        ///<returns> True if the files are loaded correctly. False if not</returns>
+        ///<remarks></remarks>
+        public static bool loadInputFiles() {
+
+            ///<summary>Load projects.csv file</summary>
+            GlobalVariables.PROJECTS_TABLE  = CommonUtilityFunctions.GetDataTableFromCsv(GlobalVariables.BASE_FOLDER_PATH + GlobalVariables.PROJECTS_FILENAME, true);
+
+            ///<summary>Load CAD.csv file</summary>
+            GlobalVariables.CAD_TABLE = CommonUtilityFunctions.GetDataTableFromCsv(GlobalVariables.BASE_FOLDER_PATH + GlobalVariables.PROJECTS_FILENAME, false);
+
+            ///<summary>Load untouchables.csv file</summary>
+            GlobalVariables.UNTOUCHABLES_TABLE = CommonUtilityFunctions.GetDataTableFromCsv(GlobalVariables.BASE_FOLDER_PATH + GlobalVariables.UNTOUCHABLES_FILENAME, false);
+
+            ///<summary>Check the variables and return file load status</summary>
+            try
+            {
+                if (GlobalVariables.PROJECTS_TABLE.IsInitialized & GlobalVariables.CAD_TABLE.IsInitialized && GlobalVariables.UNTOUCHABLES_TABLE.IsInitialized)
+                    return true;
+                else
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        
+    }
+}
